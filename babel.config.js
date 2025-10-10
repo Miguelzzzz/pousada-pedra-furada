11568b38dc42ca3846e6f570f5b4a99a86ceba12
Miguelzzzz <Miguelzzzz>
2025-10-10 11:27:26 -0300

    fix: Reconfigured Jest.setup.js and made additional configurations

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    "next/babel",          // Faz o Jest entender Next.js e ES Modules
-    "@babel/preset-react"  // Faz o Jest entender JSX
-  ]
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
-/** @type {import('jest').Config} */ 
-const config = { 
-  testEnvironment: "jest-environment-jsdom", 
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], 
-  moduleNameMapper: { 
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy", 
-  }, 
-}; 
-module.exports = config;
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+};
+module.exports = config; 
