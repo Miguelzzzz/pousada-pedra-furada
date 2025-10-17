5e60c04f76cb671b04504cc2c5997fc4a63b1dd5
Miguelzzzz <miguelmarcondesloureiro2305@gmail.com>
2025-10-17 10:18:21 -0300

    feat: Creating main.test and its respective codes

--- src/tests/pages/App.test.jsx
+++ src/tests/pages/App.test.jsx
@@ -1,5 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../../pages/index";
+jest.mock("../../pages/LoginPage", () => () => <div>Login Mock</div>); 
+jest.mock("../../pages/TarefasPage", () => () => <div>Tarefas Mock</div>); 
+jest.mock("../../pages/RelatoriosPage", () => () => <div>Relatórios Mock</div>);
+
 describe("Página inicial", () => {
   it("deve renderizar o título principal", () => {
     const { container } = render(<Home />);
@@ -8,4 +12,5 @@ describe("Página inicial", () => {
       screen.getByText("Bem-vindo à Pousada Pedra Furada")
     ).toBeInTheDocument();
   });
+    // expect(screen.getByText("Tarefas Mock")).toBeInTheDocument(); 
 }); 
