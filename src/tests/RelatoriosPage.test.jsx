0bbb85138f3b6db080ae0b5547e0d7b989b036c4
Miguelzzzz <miguelmarcondesloureiro2305@gmail.com>
2025-10-24 11:09:51 -0300

    feat: implementing Relatorios tests

--- src/tests/RelatoriosPage.test.jsx
+++ src/tests/RelatoriosPage.test.jsx
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react"; 
 import RelatoriosPage from "../pages/RelatoriosPage.jsx"; 
  
-test("deve renderizar os dados do relatório", () => { 
-  render(<RelatoriosPage />); 
-  expect(screen.getByText("Total de tarefas: 10")).toBeInTheDocument(); 
-  expect(screen.getByText("Concluídas: 6")).toBeInTheDocument(); 
+ 
+describe("RelatoriosPage.jsx — Exibição de relatórios de desempenho", () => { 
+  test("deve renderizar os dados do relatório corretamente", () => { 
+    render(<RelatoriosPage />); 
+ 
+    expect(screen.getByText("Total de tarefas: 10")).toBeInTheDocument(); 
+    expect(screen.getByText("Concluídas: 6")).toBeInTheDocument(); 
+    expect(screen.getByText("Em andamento: 3")).toBeInTheDocument(); 
+    expect(screen.getByText("Pendentes: 1")).toBeInTheDocument(); 
+  }); 
 });
