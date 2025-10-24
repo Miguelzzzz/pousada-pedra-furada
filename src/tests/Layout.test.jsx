18f92be8ac91be4d7330427937962ab5aa320e73
Miguelzzzz <miguelmarcondesloureiro2305@gmail.com>
2025-10-24 09:38:50 -0300

    test: Creation of the Layout.test.jsx and Footer.test.jsx tests and adjustment of the Layout.test.jsx test.

--- src/tests/Layout.test.jsx
+++ src/tests/Layout.test.jsx
@@ -1,17 +1,14 @@
-import { render, screen } from "@testing-library/react"; 
-import Layout from "../components/Layout.jsx"; 
- 
-jest.mock("../components/Header.jsx", () => () => <header>Mock Header</header>); 
- 
+ import { render, screen } from "@testing-library/react"; 
+import Layout from "../components/Layout"; 
+jest.mock("../components/Header", () => () => <header>Mock Header</header>); 
+jest.mock("../components/Footer", () => () => <footer>Mock Footer</footer>); 
+
 describe("Layout.jsx — Estrutura principal da aplicação", () => { 
-  it("deve renderizar o Header, o conteúdo filho e o Footer", () => { 
-    render( 
-      <Layout> 
-        <p>Conteúdo interno</p> 
-      </Layout> 
-    ); 
+  it("deve renderizar Header, conteúdo filho e Footer", () => { 
+    render(<Layout><p>Conteúdo interno</p></Layout>); 
+
     expect(screen.getByText("Mock Header")).toBeInTheDocument(); 
     expect(screen.getByText("Conteúdo interno")).toBeInTheDocument(); 
-    expect(screen.getByText(/Pousada Pedra Furada/i)).toBeInTheDocument(); 
+    expect(screen.getByText("Mock Footer")).toBeInTheDocument(); 
   }); 
 }); 
